711995a750ca7233bc94295e5f69d3f7424778ed
Boyan Xu <boyanxu@mbp16.local>
2023-06-15 00:42:42 -0700

    added sliding window l2 distance

--- methods.py
+++ methods.py
@@ -7,6 +7,9 @@ import numpy as np
 from Bio.PDB import *
 from scipy.ndimage import gaussian_filter1d, gaussian_filter
 
+from scipy import linalg, sparse, stats
+
+
 import matplotlib.pyplot as plt
 
 def make_dir(*argv):
@@ -129,6 +132,21 @@ def median_slope(data, small, big):
             print(bin_index, n_bins, s, a, b)
         
     return a + (np.argmax(scores) / n_bins) * (b - a), scores
+
+#similar to above but using mode instead of median
+def mode_slope(data, small, big):
+    slopes = []
+    weights = []
+    for i in range(len(data) - small):
+        for j in range(i + small, min(i + big, len(data))):
+            s = (data[j]-data[i])/(j-i)
+            slopes.append(s)
+    a = min(slopes)
+    b = max(slopes)    
+    n_bins = int(np.sqrt(len(slopes)))
+    bins = np.linspace(a, b, n_bins)
+    m, c = stats.mode(np.digitize(slopes,bins))
+    return bins[m][0]
 
 # regression on winding number graph of backbone curve
 def get_regression(preX):
@@ -258,6 +276,100 @@ def plot_regression_multi(winding, params, slope, save = False, filename = ''):
         plt.close()
     else:
         plt.show()
+
+#return winding and swl2 from two eigenvectors
+def get_winding_swl2(s, c):
+    ds = gaussian_filter(s, 1, order = 1)
+    dc = gaussian_filter(c, 1, order = 1)
+    r2 = s ** 2 + c ** 2
+    summand = (c * ds - s * dc) / r2    
+    winding = np.cumsum(summand) / (2 * np.pi)
+    winding *= np.sign(winding[-1] - winding[0])
+
+    N = len(winding)
+    slope = mode_slope(winding, 20, 30)
+    # slope = median_slope(winding, 20, 30)[0]
+
+    M = 10
+    swl2 = []
+    for ii in range(len(winding) - M-1):
+        res = winding[ii:ii+M]-slope*np.arange(ii,ii+M)
+        res -= np.mean(res)
+        swl2.append(np.linalg.norm(res))
+
+    return winding, swl2, slope
+
+#remaining functions written by Chris for graph laplacian calculation
+
+def get_curv_vectors(X, MaxOrder, sigma, loop = False, m = 'nearest'):
+
+    from scipy.ndimage import gaussian_filter1d as gf1d
+    if loop:
+        m = 'wrap'
+    XSmooth = gf1d(X, sigma, axis=0, order = 0, mode = m)
+    Vel = gf1d(X, sigma, axis=0, order = 1, mode = m)
+    VelNorm = np.sqrt(np.sum(Vel**2, 1))
+    VelNorm[VelNorm == 0] = 1
+    Curvs = [XSmooth, Vel]
+    for order in range(2, MaxOrder+1):
+        Tors = gf1d(X, sigma, axis=0, order=order, mode = m)
+        for j in range(1, order):
+            #Project away other components
+            NormsDenom = np.sum(Curvs[j]**2, 1)
+            NormsDenom[NormsDenom == 0] = 1
+            Norms = np.sum(Tors*Curvs[j], 1)/NormsDenom
+            Tors = Tors - Curvs[j]*Norms[:, None]
+        Tors = Tors/(VelNorm[:, None]**order)
+        Curvs.append(Tors)
+    return Curvs
+
+
+def sliding_window(dist, win):
+    N = dist.shape[0]
+    dist_stack = np.zeros((N-win+1, N-win+1))
+    for i in range(0, win):
+        dist_stack += dist[i:i+N-win+1, i:i+N-win+1]
+    for i in range(N-win+1):
+        dist_stack[i, i] = 0
+    return dist_stack
+
+def get_csm(X, Y):
+    if len(X.shape) == 1:
+        X = X[:, None]
+    if len(Y.shape) == 1:
+        Y = Y[:, None]
+    C = np.sum(X**2, 1)[:, None] + np.sum(Y**2, 1)[None, :] - 2*X.dot(Y.T)
+    C[C < 0] = 0
+    return np.sqrt(C)
+
+
+def csm_to_binary(D, kappa):
+    N = D.shape[0]
+    M = D.shape[1]
+    if kappa == 0:
+        return np.ones_like(D)
+    elif kappa < 1:
+        NNeighbs = int(np.round(kappa*M))
+    else:
+        NNeighbs = kappa
+    J = np.argpartition(D, NNeighbs, 1)[:, 0:NNeighbs]
+    I = np.tile(np.arange(N)[:, None], (1, NNeighbs))
+    V = np.ones(I.size)
+    [I, J] = [I.flatten(), J.flatten()]
+    ret = sparse.coo_matrix((V, (I, J)), shape=(N, M), dtype=np.uint8)
+    return ret.toarray()
+
+def csm_to_binary_mutual(D, kappa):
+    return csm_to_binary(D, kappa)*(csm_to_binary(D.T, kappa).T)
+
+def getUnweightedLaplacianEigsDense(W):
+    D = sparse.dia_matrix((W.sum(1).flatten(), 0), W.shape).toarray()
+    L = D - W
+    try:
+        _, v = linalg.eigh(L)
+    except:
+        return np.zeros_like(W)
+    return v
 
 '''
     plot_regression(winding, parameters, m, save = True, filename = 'fig/piecewise_disc/' + protid)
